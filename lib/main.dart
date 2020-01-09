f6305427a3955190e4612cad5c3a173cbc6f2c11
Devin Price <devin.price@nanoglobal.com>
2020-01-09 16:18:13 -0600

    Set preferred orientations.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:gad7/styles/theme.dart';
 import 'package:gad7/screens/overview.dart';
@@ -10,19 +11,26 @@ import 'package:gad7/screens/notifications.dart';
 import 'package:gad7/screens/privacy.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        initialRoute: '/',
-        theme: themeData,
-        routes: {
-          '/': (context) => OverviewScreen(),
-          '/instructions': (context) => InstructionsScreen(),
-          '/questionaire': (context) => QuestionaireScreen(),
-          '/entries': (context) => EntriesScreen(),
-          '/about': (context) => AboutScreen(),
-          '/notifications': (context) => NotificationsScreen(),
-          '/privacy': (context) => PrivacyScreen(),
-        },
-      ),
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then(
+    (_) {
+      runApp(
+        MaterialApp(
+          initialRoute: '/',
+          theme: themeData,
+          routes: {
+            '/': (context) => OverviewScreen(),
+            '/instructions': (context) => InstructionsScreen(),
+            '/questionaire': (context) => QuestionaireScreen(),
+            '/entries': (context) => EntriesScreen(),
+            '/about': (context) => AboutScreen(),
+            '/notifications': (context) => NotificationsScreen(),
+            '/privacy': (context) => PrivacyScreen(),
+          },
+        ),
+      );
+    },
   );
 }
